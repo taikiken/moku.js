34d2757d1497ca612db364fd542ac6cee9379e33
taikiken <o@parachute-canopy.org>
2016-07-14 17:41:01 +0900

    test code, so many bug

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 var assert = require('assert');
 
+var MOKU = require('../app/assets/js/bundle/moku');
+
 describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+  describe('#method()', function() {
+    it('should return true is function', function() {
+      assert.equal(true, function() {});
     });
   });
 });
