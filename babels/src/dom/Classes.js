8624c1a16b0021fc107fc763d6503ea6ec862bcd
taikiken <denkosekka@gmail.com>
2016-11-22 15:36:50 +0900

    Css 操作系クラス追加 #18

---
+++ babels/src/dom/Classes.js
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2011-2016 inazumatv.com, inc.
+ * @author (at)taikiken / http://inazumatv.com
+ * @date 2016/11/22 - 11:38
+ *
+ * Distributed under the terms of the MIT license.
+ * http://www.opensource.org/licenses/mit-license.html
+ *
+ * This notice shall be included in all copies or substantial portions of the Software.
+ *
+ */
+
+/**
+ * Element の CSS class を操作します
+ */
+export default class Classes {
+  /**
+   * 操作対象 Element を保持します
+   * @param {Element} element 操作対象 Element
+   */
+  constructor(element) {
+    /**
+     * 操作対象 Element
+     * @returns {Element} 操作対象 Element
+     */
+    this.element = () => element;
+  }
+  /**
+   * className を有するかを調べます
+   * @param {string} className 調査対象 CSS class name
+   * @returns {boolean} 存在すると true を返します
+   */
+  has(className) {
+    return Classes.has(this.element(), className);
+  }
+  /**
+   * className を追加します
+   * @param {string} className 追加対象 CSS class name
+   * @returns {boolean} 追加に成功すると true を返します
+   */
+  add(className) {
+    return Classes.add(this.element(), className);
+  }
+  /**
+   * className を削除します
+   * @param {string} className 削除対象 CSS class name
+   * @returns {boolean} 削除に成功すると true を返します
+   */
+  remove(className) {
+    return Classes.remove(this.element(), className);
+  }
+  /**
+   * Elementが引数 className を有するかを調べます
+   * @param {Element} element 操作対象 Element
+   * @param {string} className 調査対象 CSS class name
+   * @returns {boolean} 存在すると true を返します
+   */
+  static has(element, className) {
+    return Array.from(element.classList).indexOf(className) !== -1;
+  }
+  /**
+   * Element へ引数 className を追加します
+   * @param {Element} node 操作対象 Element
+   * @param {string} className 追加対象 CSS class name
+   * @returns {boolean} 追加に成功すると true を返します
+   */
+  static add(node, className) {
+    // CSS class の存在チェック
+    if (Classes.has(node, className)) {
+      return false;
+    }
+    // argument copy
+    const element = node;
+    // @type {Array<string>} - element class を取得し配列へ変換
+    const elementClasses = Array.from(element.classList);
+    // 置換え配列最後尾に新規 `className` を追加します
+    elementClasses.push(className);
+    // 配列を ' '（ワンスペース）でつなぎ文字列変換後に設定します
+    element.className = elementClasses.join(' ');
+    return true;
+  }
+  /**
+   * Element から引数 className を削除します
+   * @param {Element} node 操作対象 Element
+   * @param {string} className 削除対象 CSS class name
+   * @returns {boolean} 削除に成功すると true を返します
+   */
+  static remove(node, className) {
+    if (!Classes.has(node, className)) {
+      return false;
+    }
+    // argument copy
+    const element = node;
+    // @type {Array<string>} - element class を取得し配列へ変換
+    const elementClasses = Array.from(element.classList);
+    // 配列での削除対象 class の位置を取得します
+    const index = elementClasses.indexOf(className);
+    // 配列位置を元に削除実行します
+    const regular = elementClasses.splice(index, 1);
+    // 削除後配列を ' '（ワンスペース）でつなぎ文字列変換後に設定します
+    element.className = regular.join(' ');
+    return true;
+  }
+}
---
+++ babels/src/util/Text.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2011-2016 inazumatv.com, inc.
+ * @author (at)taikiken / http://inazumatv.com
+ * @date 2016/11/22 - 13:53
+ *
+ * Distributed under the terms of the MIT license.
+ * http://www.opensource.org/licenses/mit-license.html
+ *
+ * This notice shall be included in all copies or substantial portions of the Software.
+ *
+ */
+
+/**
+ * 文字列操作の utility
+ */
+export default class Text {
+  /**
+   * camel case を hyphenation に変換します
+   * @param {string} str 操作対象文字列
+   * @returns {string} キャメルケースをハイフネーションに変換後文字列を返します
+   */
+  static dash(str) {
+    return str.replace(/([A-Z])/g, '-$1').toLowerCase();
+  }
+  /**
+   * camel case へ変換します
+   * @param {string} str 操作対象文字列
+   * @returns {*|void|string|XML} キャメルケース文字列を返します
+   */
+  static camel(str) {
+    return str.replace(/^\s+|\s+$/g, '');
+  }
+}
