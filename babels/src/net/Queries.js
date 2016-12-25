81160d209858855d71165728c71fd46117354447
taikiken <okubo@inazumatv.com>
2016-12-25 18:13:23 +0900

    Queries 追加 #28
    TODO: test

---
+++ babels/src/net/Queries.js
@@ -0,0 +1,132 @@
+/**
+ * @license inazumatv.com
+ * @author (at)taikiken / http://inazumatv.com
+ * @date 2016/12/23
+ *
+ * Copyright (c) 2011-2015 inazumatv.com, inc.
+ *
+ * Distributed under the terms of the MIT license.
+ * http://www.opensource.org/licenses/mit-license.html
+ *
+ * This notice shall be included in all copies or substantial portions of the Software.
+ */
+
+/**
+ * URL query をパースします
+ */
+export default class Queries {
+  /**
+   * URL query を受取パースします
+   * @param {string} [queryString=location.search] パースする URL 文字列
+   */
+  constructor(queryString = window.location.search) {
+    const [data, keys] = Queries.parse(queryString);
+    const naked = Queries.naked(queryString);
+    /**
+     * query key を取得します
+     * @returns {Array<string>} query key array
+     */
+    this.keys = () => keys;
+    /**
+     * key: value 形式を取得します
+     * @returns {Object} URL query を key: value 形式で返します
+     */
+    this.data = () => data;
+    /**
+     * query 文字列を取得します
+     * @returns {string} パースする query 文字列
+     */
+    this.queryString = () => queryString;
+    /**
+     * パースしやすいように正規化した query 文字列
+     * @returns {string} ? 以降文字 + `&amp;` を `&` へ置換えます
+     */
+    this.naked = () => naked;
+  }
+  // ----------------------------------------
+  // METHOD
+  // ----------------------------------------
+  /**
+   * key が存在するかを調べます
+   * @param {string} keyName 調査対象 key 名称
+   * @returns {boolean} true: 存在する
+   */
+  has(keyName) {
+    return this.keys().indexOf(keyName) !== -1;
+  }
+  /**
+   * key 値を取得します
+   * @param {string} keyName 調査対象 key 名称
+   * @returns {string|undefined} 見つかると文字列で返します, 見つからない時は undefined を返します
+   */
+  get(keyName) {
+    return this.data()[keyName];
+  }
+  /**
+   * key: value 形式を取得します
+   * @returns {Object} URL query を key: value 形式で返します
+   */
+  getAll() {
+    return this.data();
+  }
+  // ----------------------------------------
+  // STATIC METHOD
+  // ----------------------------------------
+  /**
+   * `&amp;` を `&` へ置換えます
+   * @param {string} targetText 操作対象文字列
+   * @returns {string} `&amp;` を `&` へ置換え返します
+   */
+  static amp(targetText) {
+    return targetText.split('&amp;').join('&');
+  }
+
+  /**
+   * 文字列先頭に `?` があればそれ以降の文字列を返し `Query.amp` を実行します
+   * @param {string} targetText 操作対象文字列
+   * @returns {string} query を正規化します
+   */
+  static naked(targetText) {
+    const queryString = Queries.amp(targetText);
+    return queryString.substr(0, 1) === '?' ? queryString.substring(1) : string;
+  }
+  /**
+   * query を kye: value 形式にします
+   * @param targetText
+   * @returns {[*,*]}
+   */
+  static parse(targetText) {
+    const query = Queries.naked(targetText);
+    const pairs = query.split('&');
+    const data = {};
+    const keys = pairs.map((pair) => {
+      if (pair && pair.indexOf('=') !== -1) {
+        const keyValue = pair.split('=');
+        const key = keyValue.shift();
+        data[keys] = keyValue.shift();
+        return key;
+      }
+    });
+
+    return [data, keys];
+  }
+  /**
+   * 引数 targetText (query) から引数 keyName 値を取得します
+   * @param {string} keyName key 名称
+   * @param {string} targetText query
+   * @returns {string|undefined} 見つかると文字列で返します, 見つからない時は undefined を返します
+   */
+  static get(keyName, targetText = window.location.search) {
+    const [data] = Queries.parse(targetText);
+    return data[keyName];
+  }
+  /**
+   * URL query の key: value 形式を取得します
+   * @param {string} targetText query
+   * @returns {(*|*)[]} URL query を key: value 形式で返します
+   */
+  static getAll(targetText = window.location.search) {
+    const [data] = Queries.parse(targetText);
+    return Queries.parse(targetText);
+  }
+}
