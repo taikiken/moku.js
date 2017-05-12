80d78227f95187bb362ed701a307ea01554d7fc1
taikiken <denkosekka@gmail.com>
2017-05-12 17:52:41 +0900

    v0.2

--- libs/moku.js
+++ libs/moku.js
@@ -5918,8 +5918,8 @@ WheelEvents = function (_Events) {(0, _inherits3.default)(WheelEvents, _Events);
  * http://www.opensource.org/licenses/mit-license.html
  *
  * This notice shall be included in all copies or substantial portions of the Software.
- * 0.1.0
- * 2017-5-12 17:49:04
+ * 0.2.0
+ * 2017-5-12 17:52:06
  */
 // use strict は本来不要でエラーになる
 // 無いと webpack.optimize.UglifyJsPlugin がコメントを全部削除するので記述する
@@ -5979,10 +5979,10 @@ var MOKU = {}; /**
                 */ // dom
 // util
 // tick
-MOKU.version = function () {return '0.1.0';}; /**
+MOKU.version = function () {return '0.2.0';}; /**
                                                    * build 日時を取得します
                                                    * @returns {string}  build 日時を返します
-                                                   */MOKU.build = function () {return '2017-5-12 17:49:04';};
+                                                   */MOKU.build = function () {return '2017-5-12 17:52:06';};
 /**
                                                                                                         * MOKU.event
                                                                                                         * @type {Object} MOKU.event object を返します
